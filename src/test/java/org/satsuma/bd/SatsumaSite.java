ac69c86a2b74e4b3187b83751c64337f82d13e53
ykarpiuk <yuriy.karpiuk@zoral.com.ua>
2015-11-04 13:01:44 +0200

    Initial commit

---
+++ src/test/java/org/satsuma/bd/SatsumaSite.java
@@ -0,0 +1,24 @@
+package org.satsuma.bd;
+
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.open;
+
+
+/**
+ * Created by ykarpiuk on 11/4/2015.
+ */
+public class SatsumaSite {
+
+    //-Dtest=SatsumaSite -Dbrowser=org.openqa.selenium.chrome.ChromeDriver -Dwebdriver.chrome.driver=c:\satsumabd\chromedriver.exe test
+    //-Dtest=SatsumaSite -Dbrowser=org.openqa.selenium.htmlunit.HtmlUnitDriver test
+
+    @Test
+    public void fillPage() {
+        open("http://google.com");
+
+
+    }
+
+}
+
